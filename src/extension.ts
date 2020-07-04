96f71727eb8f7c363a072350926472e479cd4a27
Alejandro Carlstein <acarlstein@iqn.com>
2020-07-04 13:40:05 -0600

    AC1 Moved all the completion examples outside to make extension.ts cleaner and easier to understand

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as cts from './completionTypeExamples';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -8,35 +9,16 @@ export function activate(context: vscode.ExtensionContext) {
 
 	const provider1 = vscode.languages.registerCompletionItemProvider('plaintext', {
 
-		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+		provideCompletionItems(document: vscode.TextDocument, 
+													 position: vscode.Position, 
+													 token: vscode.CancellationToken, 
+													 context: vscode.CompletionContext) {
 
-			// a simple completion item which inserts `Hello World!`
-			const simpleCompletion = new vscode.CompletionItem('Hello World!');
+			const simpleCompletion = cts.getSimpleCompletion('Hello World!');
+			const snippetCompletion = cts.getSnippetCompletion();
+			const commitCharacterCompletion = cts.getCommitCharacterCompletion();
+			const commandCompletion = cts.getCommandCompletion();
 
-			// a completion item that inserts its text as snippet,
-			// the `insertText`-property is a `SnippetString` which will be
-			// honored by the editor.
-			const snippetCompletion = new vscode.CompletionItem('Good part of the day');
-			snippetCompletion.insertText = new vscode.SnippetString('Good ${1|morning,afternoon,evening|}. It is ${1}, right?');
-			snippetCompletion.documentation = new vscode.MarkdownString("Inserts a snippet that lets you select the _appropriate_ part of the day for your greeting.");
-
-			// a completion item that can be accepted by a commit character,
-			// the `commitCharacters`-property is set which means that the completion will
-			// be inserted and then the character will be typed.
-			const commitCharacterCompletion = new vscode.CompletionItem('console');
-			commitCharacterCompletion.commitCharacters = ['.'];
-			commitCharacterCompletion.documentation = new vscode.MarkdownString('Press `.` to get `console.`');
-
-			// a completion item that retriggers IntelliSense when being accepted,
-			// the `command`-property is set which the editor will execute after 
-			// completion has been inserted. Also, the `insertText` is set so that 
-			// a space is inserted after `new`
-			const commandCompletion = new vscode.CompletionItem('new');
-			commandCompletion.kind = vscode.CompletionItemKind.Keyword;
-			commandCompletion.insertText = 'new ';
-			commandCompletion.command = { command: 'editor.action.triggerSuggest', title: 'Re-trigger completions...' };
-
-			// return all completion items as array
 			return [
 				simpleCompletion,
 				snippetCompletion,
