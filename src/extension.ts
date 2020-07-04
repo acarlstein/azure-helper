e81ecdb1426b388c935e3bfb62b19c01927f3086
Alejandro Carlstein <acarlstein@iqn.com>
2020-07-04 13:48:08 -0600

    AC1 Moved providers outside extension.ts, making it easier to analyze later.

--- src/extension.ts
+++ src/extension.ts
@@ -1,56 +1,14 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as cts from './completionTypeExamples';
+import * as providers from './providersExamples';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	const provider1 = vscode.languages.registerCompletionItemProvider('plaintext', {
-
-		provideCompletionItems(document: vscode.TextDocument, 
-													 position: vscode.Position, 
-													 token: vscode.CancellationToken, 
-													 context: vscode.CompletionContext) {
-
-			const simpleCompletion = cts.getSimpleCompletion('Hello World!');
-			const snippetCompletion = cts.getSnippetCompletion();
-			const commitCharacterCompletion = cts.getCommitCharacterCompletion();
-			const commandCompletion = cts.getCommandCompletion();
-
-			return [
-				simpleCompletion,
-				snippetCompletion,
-				commitCharacterCompletion,
-				commandCompletion
-			];
-		}
-	});
-
-	const provider2 = vscode.languages.registerCompletionItemProvider(
-		'plaintext',
-		{
-			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
-
-				// get all text until the `position` and check if it reads `console.`
-				// and if so then complete if `log`, `warn`, and `error`
-				const linePrefix = document.lineAt(position).text.substr(0, position.character);
-				if (!linePrefix.endsWith('console.')) {
-					return undefined;
-				}
-
-				return [
-					new vscode.CompletionItem('log', vscode.CompletionItemKind.Method),
-					new vscode.CompletionItem('warn', vscode.CompletionItemKind.Method),
-					new vscode.CompletionItem('error', vscode.CompletionItemKind.Method),
-				];
-			}
-		},
-		'.' // triggered whenever a '.' is being typed
-	);
-
-	context.subscriptions.push(provider1, provider2);
+	const multiCompletionTypes = providers.getMultiCompletionTypes();
+	const provider2 = providers.getCompletionWithOptionsProvider()
+	context.subscriptions.push(multiCompletionTypes, provider2);
 }
 
 // this method is called when your extension is deactivated
