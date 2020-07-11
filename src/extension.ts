e936c561c83604bd8cb7f31424a576d345cb0056
Alejandro Carlstein <acarlstein@iqn.com>
2020-07-10 23:05:23 -0600

    AC3 Extracted capture terminal

--- src/extension.ts
+++ src/extension.ts
@@ -4,129 +4,27 @@ import * as vscode from 'vscode';
 import * as providers from './providersExamples';
 import * as sh from './shell';
 import * as term from './terminal';
-
-let terminalData = {};
+import * as captureTerm from './captureTerminal';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	const multiCompletionTypes = providers.getMultiCompletionTypes();
 	const completionWithOptions = providers.getCompletionWithOptionsProvider();
-	const disposableShell = sh.getShellDisposable();
-	context.subscriptions.push(multiCompletionTypes, completionWithOptions, disposableShell);	
+  const disposableShell = sh.getShellDisposable();
+  const sendTextViaTerminal = term.getSendTextViaTerminal();  
+  const terminalCaptureCommand = captureTerm.getCaptureTerminal();
 
-  context.subscriptions.push(vscode.commands.registerCommand('terminalTest.sendText', () => {
-    if (term.ensureTerminalExists()){
-      term.directTerminal()?.sendText("echo 'Hola!!! Enfermera!' > output.txt 2>&1 ");	
-    } else { 
-      console.error("No Terminal Exists");
-    }
-  }));
-
-	/********************/
-	/* Terminal Capture */
-	/********************/
-	terminalData = {};
-	let options = vscode.workspace.getConfiguration('terminalCapture');
-  
-	if (options.get('enable') === false) {
-    console.log('Terminal Capture is disabled');
-    return;
-	}
-	
-	console.log('Terminal Capture extension is now active');
-
-  if (options.get('useClipboard') === false) {
-    vscode.window.terminals.forEach(t => {
-      registerTerminalForCapture(t);
-    });
-
-    vscode.window.onDidOpenTerminal(t=> {
-      registerTerminalForCapture(t);
-    });
-	}
-	
-	context.subscriptions.push(vscode.commands.registerCommand('extension.terminalCapture.runCapture', () => {
-    if (options.get('enable') === false) {
-      console.log('Command has been disabled, not running');
-    }
-
-    const terminals = <vscode.Terminal[]>(<any>vscode.window).terminals;
-    if (terminals.length <= 0) {
-      vscode.window.showWarningMessage('No terminals found, cannot run copy');
-      return;
-    }
-
-    if (options.get('useClipboard') === true) {
-      runClipboardMode();
-    } else {
-      runCacheMode();
-    }
-  }));
+  context.subscriptions.push(
+    multiCompletionTypes, 
+    completionWithOptions, 
+    disposableShell,
+    sendTextViaTerminal,
+    terminalCaptureCommand);	
 }
 
-function registerTerminalForCapture(terminal: vscode.Terminal) {
-  terminal.processId.then(terminalId => {
-		if (terminalId){
-			(<any>terminalData)[terminalId] = "";
-			(<any>terminal).onDidWriteData((data: any) => {
-				// TODO:
-				//   - Need to remove (or handle) backspace
-				//   - not sure what to do about carriage return???
-				//   - might have some odd output
-				(<any>terminalData)[terminalId] += data;
-			});	
-		} else {
-			console.error("terminalId is nt available");
-		}
-  });
-}
 
-function runClipboardMode() {
-  vscode.commands.executeCommand('workbench.action.terminal.selectAll').then(() => {
-    vscode.commands.executeCommand('workbench.action.terminal.copySelection').then(() => {
-      vscode.commands.executeCommand('workbench.action.terminal.clearSelection').then(() => {
-        vscode.commands.executeCommand('workbench.action.files.newUntitledFile').then(() => {
-          vscode.commands.executeCommand('editor.action.clipboardPasteAction');
-        });
-      });
-    });
-  });
-}
-
-function runCacheMode() {
-  let terminal = vscode.window.activeTerminal;
-  if (terminal === undefined) {
-    vscode.window.showWarningMessage('No active terminal found, can not capture');
-    return;
-  }
-
-  terminal.processId.then(terminalId => {
-    vscode.commands.executeCommand('workbench.action.files.newUntitledFile').then(() => {
-      let editor = vscode.window.activeTextEditor;
-      if (editor === undefined) {
-        vscode.window.showWarningMessage('Failed to find active editor to paste terminal content');
-        return;
-      }
-			
-			if(terminalId){
-				let cache = cleanupCacheData((<any>terminalData)[terminalId]);
-				editor.edit(builder => {
-					builder.insert(new vscode.Position(0, 0), cache);
-				});
-			} else {
-				console.error("No terminalId to obtain cache from");
-			}
-     
-    });
-  });
-}
-
-function cleanupCacheData(data: string): string {
-  return data.replace(new RegExp('\x1b\[[0-9;]*m', 'g'), '');
-}
 
 // this method is called when your extension is deactivated
 export function deactivate() {
-	terminalData = {};
 }
--- src/terminal.ts
+++ src/terminal.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
 
-export function ensureTerminalExists(): boolean {
+function ensureTerminalExists(): boolean {
 	if ((<any>vscode.window).terminals.length === 0) {
 		vscode.window.showErrorMessage('No active terminals');
 		return false;
@@ -8,7 +8,18 @@ export function ensureTerminalExists(): boolean {
 	return true;
 }
 
-export function directTerminal(): vscode.Terminal | undefined {
+function directTerminal(): vscode.Terminal | undefined {
 	const terminals = <vscode.Terminal[]>(<any>vscode.window).terminals;
 	return terminals[0];
+}
+
+function terminal(){
+	if (ensureTerminalExists()){
+		directTerminal()?.sendText("echo 'Hola!!! Enfermera!' > output.txt 2>&1 ");	
+	} else { 
+		console.error("No Terminal Exists");
+	}
+}
+export function getSendTextViaTerminal(): vscode.Disposable {
+	return vscode.commands.registerCommand('terminalTest.sendText', terminal);
 }
